bab1fce7025bda8df7ad57e8a9f8be9f96e632bb
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-09-10 07:06:05 -0400

    + added createdat/modifiedat properties to whoelsalebugmessages

--- NeoNovaAPIAdmin/Models/DbModels/WholesaleBugMessage.cs
+++ NeoNovaAPIAdmin/Models/DbModels/WholesaleBugMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NeoNovaAPIAdmin.Models.DbModels
 {
@@ -21,5 +22,11 @@ namespace NeoNovaAPIAdmin.Models.DbModels
         [Required]
         [Range(1, 5)]
         public int UrgencyRating { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime ModifiedAt { get; set; }
     }
 }
