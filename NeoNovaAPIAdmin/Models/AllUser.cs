aca0c0ad8b3e8fded68a858cac0f9124db359677
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-09-12 10:56:49 -0400

    +adds an all users view and controller action to view all users in db

---
+++ NeoNovaAPIAdmin/Models/AllUser.cs
@@ -0,0 +1,10 @@
+﻿namespace NeoNovaAPIAdmin.Models
+{
+    public class AllUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
