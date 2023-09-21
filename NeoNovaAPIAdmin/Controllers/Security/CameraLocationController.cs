2374fb6b1022283beba0ac266b618e7bf0eccbbc
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-09-21 10:50:08 -0400

    Created the Camera, Locations adn Camera Lcoations controller, and views. Added All cameras. Still have more security models/views to make

---
+++ NeoNovaAPIAdmin/Controllers/Security/CameraLocationController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
+
+namespace NeoNovaAPIAdmin.Controllers.Security
+{
+    public class CameraLocationController : CoreController
+    {
+        private readonly IConfiguration _configuration;
+
+        public CameraLocationController(IConfiguration configuration)
+            : base() // Call the base constructor with the required parameter
+        {
+            _configuration = configuration;
+        }
+
+        private async Task<IActionResult> GetViewAsync<T>(string url)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    };
+                    var items = JsonSerializer.Deserialize<List<T>>(content, options);
+                    return View(items);
+                }
+            }
+            return View(new List<T>()); // Return an empty list if the request fails
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CameraLocations()
+        {
+            string baseUrl = _configuration.GetValue<string>("NeoNovaApiBaseUrl");
+            return await GetViewAsync<Models.SecurityModels.CameraLocation>($"{baseUrl}/api/CameraLocation");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCameraLocation(Models.SecurityModels.CameraLocation cameraLocation)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                string baseUrl = _configuration.GetValue<string>("NeoNovaApiBaseUrl");
+                var json = JsonSerializer.Serialize(cameraLocation);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync($"{baseUrl}/api/CameraLocation", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Redirect to the GET action to refresh the page
+                    return RedirectToAction("CameraLocation");
+                }
+            }
+            // Handle error, e.g., return a view with an error message
+            return View("Error");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PutCameraLocation(int id, Models.SecurityModels.CameraLocation cameraLocation)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                string baseUrl = _configuration.GetValue<string>("NeoNovaApiBaseUrl");
+                var json = JsonSerializer.Serialize(cameraLocation);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await httpClient.PutAsync($"{baseUrl}/api/CameraLocation/{id}", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Redirect to the GET action to refresh the page
+                    return RedirectToAction("CameraLocation");
+                }
+            }
+            // Handle error, e.g., return a view with an error message
+            return View("Error");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCameraLocation(int id)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                string baseUrl = _configuration.GetValue<string>("NeoNovaApiBaseUrl");
+                var response = await httpClient.DeleteAsync($"{baseUrl}/api/CameraLocation/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    // Redirect to the GET action to refresh the page
+                    return RedirectToAction("CameraLocation");
+                }
+            }
+            // Handle error, e.g., return a view with an error message
+            return View("Error");
+        }
+    }
+}
---
+++ NeoNovaAPIAdmin/Models/SecurityModels/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeoNovaAPIAdmin.Models.SecurityModels
+{
+    [Table("Cameras")]
+    public class Camera
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [ForeignKey("Location")]
+        public int LocationId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string CurrentStatus { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? ModifiedAt { get; set; }
+
+        public List<CameraLocation>? CameraLocations { get; set; }
+
+    }
+}
